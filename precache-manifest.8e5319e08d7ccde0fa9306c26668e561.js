c957863908e024973529f55dfc3d75e2676b95d2
Dalton <chike911@yahoo.com>
2019-11-17 14:16:23 -0700

    Updates

---
+++ precache-manifest.8e5319e08d7ccde0fa9306c26668e561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d35aa377617e44556106a4168af388a",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b3a79619845419ea0f09",
+    "url": "/recipe-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fa1e9b01c5cf36d191a0",
+    "url": "/recipe-app/static/css/main.9dd5aafc.chunk.css"
+  },
+  {
+    "revision": "b3a79619845419ea0f09",
+    "url": "/recipe-app/static/js/2.c6d6353e.chunk.js"
+  },
+  {
+    "revision": "fa1e9b01c5cf36d191a0",
+    "url": "/recipe-app/static/js/main.d4390996.chunk.js"
+  },
+  {
+    "revision": "f45dcbc11275cd88eb53",
+    "url": "/recipe-app/static/js/runtime-main.3a5d6ccd.js"
+  }
+]);
