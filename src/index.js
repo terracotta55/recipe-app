a86c41b6562d23d4962e00b68c6468e084d0fcfc
Dalton <chike911@yahoo.com>
2019-11-17 20:31:13 -0700

    added hashrouter for gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Router from "./components/Router.jsx";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<Router />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(
+  <HashRouter>
+    <Router />
+  </HashRouter>,
+  document.getElementById("root")
+);
+// registerServiceWorker();
